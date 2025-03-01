d40292051e99046f370627a077accfc5cf70c8bc
Thomas Simmer <thomas.simmer@hotmail.fr>
2025-03-01 19:10:29 +0100

    Fix about page

--- frontend/lib/features/profile/presentation/screens/about_screen.dart
+++ frontend/lib/features/profile/presentation/screens/about_screen.dart
@@ -6,47 +6,52 @@ import 'package:url_launcher/url_launcher.dart';
 class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final Uri githubUrl = Uri.parse('https://github.com/thomassimmer');
+    final Uri githubUrl = Uri.parse('https://github.com/tatkagore');
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            AppLocalizations.of(context)!.about,
-            style: context.typographies.headingSmall,
-            overflow: TextOverflow.ellipsis,
-            maxLines: 1,
+      appBar: AppBar(
+        title: Text(
+          AppLocalizations.of(context)!.about,
+          style: context.typographies.headingSmall,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 1,
+        ),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(40.0),
+          child: Column(
+            children: [
+              Text(
+                AppLocalizations.of(context)!.aboutText,
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              TextButton(
+                onPressed: () async {
+                  if (await canLaunchUrl(githubUrl)) {
+                    await launchUrl(githubUrl,
+                        mode: LaunchMode.externalApplication,
+                        webOnlyWindowName: '_blank');
+                  } else {
+                    throw 'Could not launch $githubUrl';
+                  }
+                },
+                style: ButtonStyle(
+                    backgroundColor: WidgetStatePropertyAll(
+                        context.colors.hint.withValues(alpha: 0.1))),
+                child: Image.asset(
+                  'assets/images/github-logo.png',
+                  width: 50,
+                  height: 50,
+                ),
+              ),
+            ],
           ),
         ),
-        body: Center(
-            child: Padding(
-                padding: const EdgeInsets.all(30.0),
-                child: Column(children: [
-                  Text(
-                    AppLocalizations.of(context)!.aboutText,
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      if (await canLaunchUrl(githubUrl)) {
-                        await launchUrl(githubUrl,
-                            mode: LaunchMode.externalApplication,
-                            webOnlyWindowName: '_blank');
-                      } else {
-                        throw 'Could not launch $githubUrl';
-                      }
-                    },
-                    style: ButtonStyle(
-                        backgroundColor: WidgetStatePropertyAll(
-                            context.colors.hint.withValues(alpha: 0.1))),
-                    child: Image.asset(
-                      'assets/images/github-logo.png',
-                      width: 50,
-                      height: 50,
-                    ),
-                  ),
-                ]))));
+      ),
+    );
   }
 }
