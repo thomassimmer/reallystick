463c3dc5c7a154d49e04eb4f37f98efd542af572
Thomas Simmer <thomas.simmer@hotmail.fr>
2025-05-25 21:34:15 +0200

    Make token storage methods more robust

--- frontend/lib/features/auth/data/storage/token_storage.dart
+++ frontend/lib/features/auth/data/storage/token_storage.dart
@@ -1,28 +1,50 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:logger/web.dart';
 
 class TokenStorage {
   final _storage = FlutterSecureStorage(
     aOptions: AndroidOptions(encryptedSharedPreferences: true),
   );
+  final logger = Logger();
 
   Future<void> saveTokens(
     String accessToken,
     String refreshToken,
   ) async {
-    await _storage.write(key: 'accessToken', value: accessToken);
-    await _storage.write(key: 'refreshToken', value: refreshToken);
+    try {
+      await _storage.write(key: 'accessToken', value: accessToken);
+      await _storage.write(key: 'refreshToken', value: refreshToken);
+    } catch (e) {
+      logger.e("saveTokens: $e");
+      return;
+    }
   }
 
   Future<String?> getAccessToken() async {
-    return await _storage.read(key: 'accessToken');
+    try {
+      return await _storage.read(key: 'accessToken');
+    } catch (e) {
+      logger.e("getAccessToken: $e");
+      return null;
+    }
   }
 
   Future<String?> getRefreshToken() async {
-    return await _storage.read(key: 'refreshToken');
+    try {
+      return await _storage.read(key: 'refreshToken');
+    } catch (e) {
+      logger.e("getRefreshToken: $e");
+      return null;
+    }
   }
 
   Future<void> deleteTokens() async {
-    await _storage.delete(key: 'accessToken');
-    await _storage.delete(key: 'refreshToken');
+    try {
+      await _storage.delete(key: 'accessToken');
+      await _storage.delete(key: 'refreshToken');
+    } catch (e) {
+      logger.e("deleteTokens: $e");
+      return;
+    }
   }
 }
